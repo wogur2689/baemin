b132d382d286692619e75311a8e8519731c63041
wogur2689 <81075170+wogur2689@users.noreply.github.com>
2022-12-12 18:03:25 +0900

    장바구니 생성

---
+++ baemin/src/main/java/com/example/demo/controller/CartController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CartController {
+	
+//	@ResponseBody
+//	@PostMapping("/addCart")
+//	public
+}
